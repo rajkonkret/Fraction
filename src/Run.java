4aa059edc0f485384a931d03b1d2fd43e50eeb55
rajkonkret <rajkonkret660@gmail.com>
2019-11-19 21:18:03 +0100

    Initial commit

---
+++ src/Run.java
@@ -0,0 +1,11 @@
+public class Run {
+
+    public static void main(String[] args) {
+        //    Zdefiniuj klasę Fraction , która odpowiada za reprezentacje ułamków.
+        //    Zdefiniuj klasę Calculator i metody odpowiedzialne za dodawanie,odejmowanie
+        //    ułamków.
+
+        Fraction fraction = new Fraction(1, 2);
+        
+    }
+}
