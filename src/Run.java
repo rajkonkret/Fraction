3e8b8c964d44f208fdaa4d17a691cf1a5935be9c
rajkonkret <rajkonkret660@gmail.com>
2019-11-20 11:26:27 +0100

    dodane JFrame

--- src/Run.java
+++ src/Run.java
@@ -1,16 +1,19 @@
+import javax.swing.*;
+
 public class Run {
 
     public static void main(String[] args) {
         //    Zdefiniuj klasę Fraction , która odpowiada za reprezentacje ułamków.
         //    Zdefiniuj klasę Calculator i metody odpowiedzialne za dodawanie,odejmowanie
         //    ułamków.
-
+        JFrame window = new JFrame();
+        window.setVisible(true);
         Fraction sumFraction = new Fraction(1, 1);
         Fraction fraction = new Fraction(2, 8);
         Fraction fraction1 = new Fraction(1, 8);
         sumFraction = Fraction.add(fraction, fraction1);
-        System.out.println(fraction.getNumerator()+"   "+fraction1.getNumerator()+"   "+sumFraction.getNumerator());
+        System.out.println(fraction.getNumerator() + "   " + fraction1.getNumerator() + "   " + sumFraction.getNumerator());
         System.out.println("- + - = -");
-        System.out.println(fraction.getDenominator()+"   "+fraction1.getDenominator()+"   "+sumFraction.getDenominator());
+        System.out.println(fraction.getDenominator() + "   " + fraction1.getDenominator() + "   " + sumFraction.getDenominator());
     }
 }
