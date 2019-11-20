e9996987dfdd020d2942cd38b9adc163d02dc9c1
rajkonkret <rajkonkret660@gmail.com>
2019-11-20 22:55:17 +0100

    dodanie klasy Calculator do obliczeń - dodano mnożenie i poprawiono

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
     public Calculator() {
     }
 
-    public  Fraction add(Fraction first, Fraction second) {
+    public Fraction add(Fraction first, Fraction second) {
         int numerator = 0;
         int denominator = 1;
         if (second.getDenominator() == first.getDenominator()) {
@@ -12,13 +12,14 @@ public class Calculator {
         Fraction add = new Fraction(numerator, denominator);
         return add;
     }
-    public  Fraction sub(Fraction first, Fraction second) {
+
+    public Fraction sub(Fraction first, Fraction second) {
         int numerator = 0;
         int denominator = 1;
-        if (second.getDenominator() == first.getDenominator()) {
-            numerator = first.getNumerator() * second.getNumerator();
-            denominator = first.getDenominator() * second.getDenominator();
-        }
+
+        numerator = first.getNumerator() * second.getNumerator();
+        denominator = first.getDenominator() * second.getDenominator();
+
         Fraction add = new Fraction(numerator, denominator);
         return add;
     }
